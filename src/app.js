70fcfcec374d8be568481e2bbee00c0d864dc1a5
soheil323 <soheil.amjadi.323@gmail.com>
2023-11-12 17:12:20 +0330

    modify package.json

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 const port = process.env.PORT || 3000
+
 //Define path for Express config
 
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -85,5 +86,5 @@ app.get('*', (req,res) => {
 })
 
 app.listen(port, () => {
-    console.log('Server is up on port'+ port)
+    console.log('Server is up on port' + port)
 })
